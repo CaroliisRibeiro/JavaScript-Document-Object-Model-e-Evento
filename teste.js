70eaec2e4f04aadc331b48221e53d5b9efce81bf
Carol Ribeiro <anacalinda@gmail.com>
2024-11-05 23:28:40 -0300

    atividade finalizada

--- teste.js
+++ teste.js
@@ -1,7 +1,29 @@
-document.getElementById('texto').style.color = 'purple';
+document.getElementById('texto').style.color = 'lime';
 
 function alertaTexto(){
 
         alert("Bem-vinde ao nosso site!");
       }
 
+    
+
+function alteraCor() {
+  if(  document.body.style.backgroundColor === "blue"){
+    document.body.style.backgroundColor = "white"
+    } else{
+          document.body.style.backgroundColor = "blue";
+        }
+      }
+      
+      function exibeURLAtual(){
+        alert("Mostrar URL");
+        let origin = window.location.origin;
+
+        let novoParagrafo = document.createElement("p");
+        novoParagrafo.innerHTML = origin + ' - URL de Origem';
+        novoParagrafo.style.color = "deeppink";
+
+        document.body.appendChild(novoParagrafo);
+
+
+      }
