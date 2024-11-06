a470019cad665522c8f085a2a3770c36d4d652fc
Carol Ribeiro <anacalinda@gmail.com>
2024-11-06 01:10:30 -0300

    atividade finalizada 01

--- teste.js
+++ teste.js
@@ -6,7 +6,6 @@ function alertaTexto(){
       }
 
     
-
 function alteraCor() {
   if(  document.body.style.backgroundColor === "blue"){
     document.body.style.backgroundColor = "white"
@@ -15,15 +14,49 @@ function alteraCor() {
         }
       }
       
-      function exibeURLAtual(){
-        alert("Mostrar URL");
-        let origin = window.location.origin;
+function exibeURLAtual(){
+  alert("Mostrar URL");
+    let origin = window.location.origin;
 
-        let novoParagrafo = document.createElement("p");
+    let novoParagrafo = document.createElement("p");
         novoParagrafo.innerHTML = origin + ' - URL de Origem';
         novoParagrafo.style.color = "deeppink";
 
         document.body.appendChild(novoParagrafo);
 
 
+      }
+
+
+      function voltaPagina(){
+
+        let length = history.length;
+        document.getElementById("texto3").innerHTML = "Número de URLs na lista de histórico: " + length;
+        
+        if (length > 1) {
+            history.back(); 
+        } else {
+            alert("Não há uma página anterior no histórico.");
+        }
+    
+      }
+
+      function verificaCookies(){
+
+        if (navigator.cookieEnabled) {
+          alert("Cookies estão habilitados.");
+          document.getElementById("texto4").innerHTML = "Cookies estão habilitados no navegador.";
+      } else {
+          alert("Cookies não estão habilitados.");
+          document.getElementById("texto4").innerHTML = "Cookies não estão habilitados no navegador.";
+      }
+    }
+
+
+        function mudarCor() {
+            document.getElementById("caixa").style.backgroundColor = "blueviolet";
+        }
+
+        function restaurarCor() {
+          document.getElementById("caixa").style.backgroundColor = "Khaki";
       }
